9acadd88660916278b745f59057d46113ea0e435
ryumedia <ryumediadigital@gmail.com>
2026-02-27 11:29:27 +0700

    edit route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,20 +1,22 @@
-import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/admin/dashboard'
+  const next = searchParams.get('next') ?? '/'
 
   if (code) {
-    const supabase = await createClient()
+    const cookieStore = await cookies()
+    const supabase = createClient(cookieStore)
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${origin}`)
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/login?message=Could not authenticate user`)
+  return NextResponse.redirect(`/auth/auth-code-error`)
 }
