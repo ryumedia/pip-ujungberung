895ae28374549753229d105e17256d66f1ade2ce
ryumedia <ryumediadigital@gmail.com>
2026-02-27 14:26:13 +0700

    perbaikan

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['xlsx'],
+  },
+};
 
 export default nextConfig;
